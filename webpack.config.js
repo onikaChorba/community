a322d1fe439011e6f1f2597e9a4f5839ec8e532f
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-09-07 20:49:06 +0300

    add section in main file

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            template: path.resolve(__dirname, './src/front-page.html'), // шаблон
             filename: 'index.html', // название выходного файла
         }),
         new CleanWebpackPlugin(),
@@ -37,6 +37,10 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
+            // {
+            //     test: /\.(html)$/,
+            //     use: ['html-loader'],
+            // },
             {
                 test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
                 type: 'asset/resource',
@@ -50,5 +54,7 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             },
         ],
+
     }
+
 }
