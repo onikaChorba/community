e3bc68facda1ca72dc46cf3489928144679b9cd7
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-09-30 20:33:47 +0300

    changing strusture site

--- src/Blog.js
+++
@@ -1,4 +0,0 @@
-import './scss/Front-page.scss';
-import './scss/About-us.scss';
-import './scss/Sermons-main.scss';
-import './scss/Blog.scss';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './scss/Front-page.scss';
+import './js/front-page.js';
+
 import './scss/About-us.scss';
+
 import './scss/Sermons-main.scss';
-import './scss/Sermons-event.scss';
+
 import './scss/Blog.scss';
+
+import './scss/contact-us.scss';
