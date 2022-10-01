8537efa12d48358b644a1aed42239d2f52949acf
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-10-01 13:05:45 +0300

    build site

---
+++ src/php/send.php
@@ -0,0 +1,18 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$name = urldecode($name);
+$email = urldecode($email);
+$name = trim($name);
+$email = trim($email);
+
+
+if( mail("onika164604@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
+  echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+};
+?>
